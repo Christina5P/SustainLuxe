9f75035e9130c68c3e6f08b97212ddaddb76ff18
Christina Åhman <christina.ahman@tv4.se>
2024-10-05 09:34:55 +0000

    install dj-database and changed settings

--- sustainluxe/settings.py
+++ sustainluxe/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 
 if os.path.exists('env.py'):
@@ -118,17 +119,10 @@ WSGI_APPLICATION = 'sustainluxe.wsgi.application'
 #      'NAME': BASE_DIR / 'db.sqlite3',
 # }
 # }
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
