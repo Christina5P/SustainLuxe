f09e168dce3262ae06cdaf30e1487f93c27b226c
Christina Åhman <christina.ahman@tv4.se>
2024-11-11 12:17:55 +0000

    troubleshooting webhook

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -36,15 +36,13 @@ def webhook(request):
     """
 
     try:
-        event = stripe.Webhook.construct_event(payload, sig_header, wh_secret)
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
     except ValueError as e:
-        # Invalid payload
         return HttpResponse(status=400)
     except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
         return HttpResponse(status=400)
-    except Exception as e:
-        return HttpResponse(content=e, status=400)
 
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
